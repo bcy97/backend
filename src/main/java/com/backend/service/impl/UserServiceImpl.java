ea967df390d02dfb24d4f198bfc6fa8134d8fccf
bcy97 <609233559@qq.com>
2018-07-24 23:44:54 +0800

    modify 修改userserviceimpl

--- src/main/java/com/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/backend/service/impl/UserServiceImpl.java
@@ -2,14 +2,20 @@ package com.backend.service.impl;
 
 import com.backend.service.UserService;
 import com.backend.util.Constants;
-import com.backend.util.SocketConnect;
 import com.backend.vo.DataPacket;
 import com.backend.vo.UserInfo;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -121,10 +127,58 @@ public class UserServiceImpl implements UserService {
     private ByteBuffer getUserInfo(byte type, byte[] datas) {
         ByteBuffer bb = null;
         DataPacket dp = new DataPacket(Constants.CC_USERINFO, datas);
-        byte[] bDatas = dp.serialize();
 
-        SocketConnect.getData(bb, dp, bDatas, logger);
+        Socket socket = new Socket();
+        try {
+            socket.connect(getSocketAddress());
+            OutputStream os = socket.getOutputStream();
+            InputStream is = socket.getInputStream();
 
-        return bb;
+            byte[] bDatas = dp.serialize();
+            os.write(bDatas, 0, bDatas.length);
+
+            bb = ByteBuffer.allocate(8 * 1024);
+            bb.order(ByteOrder.LITTLE_ENDIAN);
+
+            receiveData(bb, is);
+            is.close();
+            os.close();
+
+            return bb;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            logger.error(e.getMessage());
+            return null;
+        }
+
+    }
+
+
+    private SocketAddress getSocketAddress() {
+        String ip = "127.0.0.1";
+        int port = 8888;
+
+        return new InetSocketAddress(ip, port);
+    }
+
+    private void receiveData(ByteBuffer bb, InputStream is) throws IOException {
+        DataPacket dp;
+        byte[] bDatas;
+        while (true) {
+            byte[] bHead = new byte[12];
+            is.read(bHead, 0, 12);
+            dp = new DataPacket();
+            dp.toDataPacketHead(bHead);
+
+            bDatas = new byte[dp.getLength()];
+            is.read(bDatas, 0, bDatas.length);
+            bb.put(bDatas);
+
+            is.read(bDatas, 0, 1);
+
+            if (0 == dp.getTailFlag())
+                break;
+        }
     }
 }

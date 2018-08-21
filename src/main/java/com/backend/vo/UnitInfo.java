1e7470b43f8bfec1f85e7df4c7f244587ac5ca1c
bcy97 <609233559@qq.com>
2018-08-21 20:50:29 +0800

    add 添加漏掉的UnitInfo类

---
+++ src/main/java/com/backend/vo/UnitInfo.java
@@ -0,0 +1,42 @@
+package com.backend.vo;
+
+public class UnitInfo {
+
+    public UnitInfo(String name,short unitNo,byte type){
+        this.name = name;
+        this.type = type;
+        this.unitNo = unitNo;
+    }
+
+    public UnitInfo(){
+
+    }
+
+    private String name;
+    private short unitNo;
+    private byte type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getUnitNo() {
+        return unitNo;
+    }
+
+    public void setUnitNo(short unitNo) {
+        this.unitNo = unitNo;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+}

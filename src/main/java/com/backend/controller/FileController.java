cbeba33e755512371a98b33e15d99bbab9fa1467
bcy97 <609233559@qq.com>
2018-09-01 10:53:24 +0800

    add 匹配MD5码

--- src/main/java/com/backend/controller/FileController.java
+++ src/main/java/com/backend/controller/FileController.java
@@ -1,18 +1,38 @@
 package com.backend.controller;
 
+import com.backend.util.MD5;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/file")
 public class FileController {
+
+    @Autowired
+    private MD5 mdUtil;
 
     public File[] download(ArrayList<String> filename) {
         return null;
     }
 
-    public boolean compare(String md5) {
+    @RequestMapping("/compare")
+    public boolean compare(@RequestBody String md5) {
+
+        try {
+            File configList = ResourceUtils.getFile("classpath:static/configList.xml");
+            if (md5 != null && md5.equals(mdUtil.getMD5(configList))) {
+                return true;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         return false;
     }

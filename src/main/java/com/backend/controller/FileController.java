c7cd933b921acea2a7d173f9b94e5783d73befa1
bcy97 <609233559@qq.com>
2018-11-03 13:33:13 +0800

    add 添加图片库列表接口

--- src/main/java/com/backend/controller/FileController.java
+++ src/main/java/com/backend/controller/FileController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.*;
 
 @RestController
 @RequestMapping("/file")
@@ -22,13 +22,14 @@ public class FileController {
         return null;
     }
 
+    String rootPath = System.getProperty("user.dir");
+
     @RequestMapping("/compare")
     public boolean compare(@RequestBody String md5) {
 
         try {
             // 取得根目录路径
-            String rootPath = System.getProperty("user.dir");
-            File configList = ResourceUtils.getFile(rootPath + "/static/configList.xml");
+            File configList = ResourceUtils.getFile(rootPath + "/baseConfigs/configList.xml");
             if (md5 != null && md5.equals(mdUtil.getMD5(configList))) {
                 return true;
             }
@@ -37,6 +38,19 @@ public class FileController {
         }
 
         return false;
+    }
+
+    @RequestMapping("/getIconLibrary")
+    public Map<String, List<String>> getIconLibrary() {
+        File publicLibrary = new File(rootPath + "/iconlibrary/public");
+        File userLibrary = new File(rootPath + "/iconlibrary/user");
+
+        Map<String, List<String>> picList = new HashMap<>();
+        picList.put("public", Arrays.asList(publicLibrary.list()));
+        picList.put("user", Arrays.asList(userLibrary.list()));
+
+        return picList;
+
     }
 
 }

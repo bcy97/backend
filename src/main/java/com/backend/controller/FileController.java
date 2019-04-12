d21afd44dcb49fd38b57f574992ecc37f3214705
allenLin11 <1169539121@qq.com>
2019-04-12 12:03:47 +0800

    2019-04-12/1111

--- src/main/java/com/backend/controller/FileController.java
+++ src/main/java/com/backend/controller/FileController.java
@@ -18,18 +18,15 @@ public class FileController {
     @Autowired
     private MD5 mdUtil;
 
-    public File[] download(ArrayList<String> filename) {
-        return null;
-    }
-
     String rootPath = System.getProperty("user.dir");
 
     @RequestMapping("/compare")
-    public boolean compare(@RequestBody String md5) {
+    public boolean compare(@RequestBody Map<String,String> data) {
 
         try {
+            String md5 = data.get("md5");
             // 取得根目录路径
-            File configList = ResourceUtils.getFile(rootPath + "/baseConfigs/configList.xml");
+            File configList = ResourceUtils.getFile(rootPath + data.get("companyId") + "/baseConfigs/configList.xml");
             if (md5 != null && md5.equals(mdUtil.getMD5(configList))) {
                 return true;
             }
@@ -41,13 +38,13 @@ public class FileController {
     }
 
     @RequestMapping("/getIconLibrary")
-    public Map<String, List<String>> getIconLibrary() {
+    public Map<String, List<String>> getIconLibrary(Map<String,String> data) {
         File publicLibrary = new File(rootPath + "/iconlibrary/public");
-        File userLibrary = new File(rootPath + "/iconlibrary/user");
+        File userLibrary = new File(rootPath + "/iconlibrary/" + data.get("companyId"));
 
         Map<String, List<String>> picList = new HashMap<>();
         picList.put("public", Arrays.asList(publicLibrary.list()));
-        picList.put("user", Arrays.asList(userLibrary.list()));
+        picList.put(data.get("companyId"), Arrays.asList(userLibrary.list()));
 
         return picList;
 
